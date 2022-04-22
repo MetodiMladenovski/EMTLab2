caa0aac9d472407fbd7d7cccfe85d5dbc72e97c8
Metodi Mladenovski <meto.18@outlook.com>
2022-04-23 01:08:15 +0200

    Implemented React Frontend App

--- src/main/java/com/example/emtlab2/controllersRest/AuthorController.java
+++ src/main/java/com/example/emtlab2/controllersRest/AuthorController.java
@@ -2,6 +2,7 @@ package com.example.emtlab2.controllersRest;
 
 import com.example.emtlab2.model.Author;
 import com.example.emtlab2.services.AuthorService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 @RequestMapping("/authors")
 public class AuthorController {
 
